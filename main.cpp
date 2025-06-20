415d2c851110ed6c1cb764c7881ae4217d2e3db8
Douglas Dias <douglas.dias2102@outlook.com>
2025-06-19 23:01:50 -0300

    Adding a Hello World to have the minimum configuration

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // params: width, height, name of the window, fullscreen, not important
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Learning OpenGL", NULL, NULL);
+
+    if(window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
